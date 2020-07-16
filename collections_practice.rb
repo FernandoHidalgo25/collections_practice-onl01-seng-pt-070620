48e5dcaa30b5b45246738eb7917d2cfdf7763ffb
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-07-16 17:00:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,7 +40,7 @@ def sum_array(array)
   end
 
 def add_s(array)
-  array.each_with_index.collect do |element, index|
+  array.map_with_index do |element, index|
         if index == 1
             element
         else element[element.length] = "s"
